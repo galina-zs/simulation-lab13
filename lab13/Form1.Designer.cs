f43f97684985d4bbf6ca24ff1f95a9438bd4795e
galina-zs <zab.galina.24@gmail.com>
2021-05-17 22:45:07 +0300

    Task is done

---
+++ lab13/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.paramLabel = new System.Windows.Forms.Label();
+            this.paramNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.numberOfExperimentsLabel = new System.Windows.Forms.Label();
+            this.numberOfExperimentsNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.startButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.averageLabel = new System.Windows.Forms.Label();
+            this.varianceLabel = new System.Windows.Forms.Label();
+            this.chiLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.paramNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfExperimentsNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paramLabel
+            // 
+            this.paramLabel.AutoSize = true;
+            this.paramLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.paramLabel.Location = new System.Drawing.Point(25, 41);
+            this.paramLabel.Name = "paramLabel";
+            this.paramLabel.Size = new System.Drawing.Size(64, 24);
+            this.paramLabel.TabIndex = 0;
+            this.paramLabel.Text = "Param";
+            // 
+            // paramNumericUpDown
+            // 
+            this.paramNumericUpDown.DecimalPlaces = 3;
+            this.paramNumericUpDown.Location = new System.Drawing.Point(134, 47);
+            this.paramNumericUpDown.Name = "paramNumericUpDown";
+            this.paramNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.paramNumericUpDown.TabIndex = 2;
+            this.paramNumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numberOfExperimentsLabel
+            // 
+            this.numberOfExperimentsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberOfExperimentsLabel.Location = new System.Drawing.Point(12, 86);
+            this.numberOfExperimentsLabel.Name = "numberOfExperimentsLabel";
+            this.numberOfExperimentsLabel.Size = new System.Drawing.Size(116, 57);
+            this.numberOfExperimentsLabel.TabIndex = 3;
+            this.numberOfExperimentsLabel.Text = "Number of experiments";
+            // 
+            // numberOfExperimentsNumericUpDown
+            // 
+            this.numberOfExperimentsNumericUpDown.Location = new System.Drawing.Point(134, 111);
+            this.numberOfExperimentsNumericUpDown.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numberOfExperimentsNumericUpDown.Name = "numberOfExperimentsNumericUpDown";
+            this.numberOfExperimentsNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.numberOfExperimentsNumericUpDown.TabIndex = 4;
+            this.numberOfExperimentsNumericUpDown.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(29, 167);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(284, 41);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(647, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // averageLabel
+            // 
+            this.averageLabel.AutoSize = true;
+            this.averageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.averageLabel.Location = new System.Drawing.Point(290, 359);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(72, 20);
+            this.averageLabel.TabIndex = 7;
+            this.averageLabel.Text = "Average:";
+            // 
+            // varianceLabel
+            // 
+            this.varianceLabel.AutoSize = true;
+            this.varianceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.varianceLabel.Location = new System.Drawing.Point(290, 388);
+            this.varianceLabel.Name = "varianceLabel";
+            this.varianceLabel.Size = new System.Drawing.Size(76, 20);
+            this.varianceLabel.TabIndex = 8;
+            this.varianceLabel.Text = "Variance:";
+            // 
+            // chiLabel
+            // 
+            this.chiLabel.AutoSize = true;
+            this.chiLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chiLabel.Location = new System.Drawing.Point(290, 417);
+            this.chiLabel.Name = "chiLabel";
+            this.chiLabel.Size = new System.Drawing.Size(99, 20);
+            this.chiLabel.TabIndex = 9;
+            this.chiLabel.Text = "Chi-squared:";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 359);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 95);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Вывод: при увеличении числа экспериментов эмпирическое распредленеие стремится к " +
+    "теоретическому";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 471);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chiLabel);
+            this.Controls.Add(this.varianceLabel);
+            this.Controls.Add(this.averageLabel);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.numberOfExperimentsNumericUpDown);
+            this.Controls.Add(this.numberOfExperimentsLabel);
+            this.Controls.Add(this.paramNumericUpDown);
+            this.Controls.Add(this.paramLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.paramNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfExperimentsNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label paramLabel;
+        private System.Windows.Forms.NumericUpDown paramNumericUpDown;
+        private System.Windows.Forms.Label numberOfExperimentsLabel;
+        private System.Windows.Forms.NumericUpDown numberOfExperimentsNumericUpDown;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label averageLabel;
+        private System.Windows.Forms.Label varianceLabel;
+        private System.Windows.Forms.Label chiLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lab13/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab13
+{
+    public partial class Form1 : Form
+    {
+        double U = 1;
+        const int p = 5087; // большое простое число
+        int M = 2900;       // M = p - 3^n. Берем n = 7, потому что 3^8 > p
+        double R;
+
+        double lambda = 0;
+        int numberOfExperiments = 0;
+
+        List<double> probs = new List<double>();
+        List<double> stat = new List<double>();
+
+        double[] criticalChi = new double [20]{3.8, 6, 7.8, 9.5, 11.1, 12.6, 14.1, 15.5, 16.9, 18.3,
+                                               19.7, 21, 22.4, 23.7, 25, 26.3, 27.6, 28.9, 30.1, 31.4};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+
+            lambda = (double)paramNumericUpDown.Value;
+            numberOfExperiments = (int)numberOfExperimentsNumericUpDown.Value;
+
+            probs.Clear();
+            stat.Clear();
+
+            FindProbabilities();
+            FindStatistics();
+
+            double average = FindAverage();
+            double variance = FindVariance(average);
+
+            double averageError = Math.Abs(average - lambda) / Math.Abs(average) * 100;
+            double varianceError = Math.Abs(variance - lambda) / Math.Abs(average) * 100;
+
+            double currentCriticalChi;
+            double currentChi = FindChi();
+            if (stat.Count <= 20)
+            {
+                currentCriticalChi = criticalChi[stat.Count - 1];
+            }
+            else
+            {
+                currentCriticalChi = criticalChi[19];
+            }
+
+            if (currentCriticalChi >= currentChi)
+            {
+                chiLabel.Text = "Chi-squared: " + currentCriticalChi.ToString() + ">= " + currentChi + " hypothesis confirmed";
+            }
+            else
+            {
+                chiLabel.Text = "Chi-squared: " + currentCriticalChi.ToString() + "< " + currentChi + " hypothesis not confirmed";
+            }
+
+            for (int i = 0; i < stat.Count; i++)
+            {                
+                chart1.Series[0].Points.AddXY(i + 1, stat[i] / numberOfExperiments);
+            }
+            averageLabel.Text = "Average: " + average.ToString() +
+                                " (error = " + ((int)(averageError)).ToString() + "%)";
+            varianceLabel.Text = "Variance: " + variance.ToString() +
+                                " (error = " + ((int)(varianceError)).ToString() + "%)";
+        }
+
+        private void FindProbabilities()
+        {
+            int n = 0;
+            double minProb = 1;
+            double tempProb;
+            long factorial = 1;
+            double sumOfProbs = 0;
+
+            while (true)
+            {
+                if (1 - sumOfProbs < minProb && n >= lambda)
+                {
+                    probs.Add(1 - sumOfProbs);
+                    stat.Add(0);
+                    return;
+                }
+                if (n != 0)
+                {
+                    factorial *= n;
+                }
+                tempProb = (double)(Math.Pow(lambda, n) / factorial) * (double)Math.Exp(-lambda);
+                sumOfProbs += tempProb;
+                probs.Add(tempProb);
+                stat.Add(0);
+                if (tempProb < minProb)
+                {
+                    minProb = tempProb;
+                }
+                n++;
+            }
+        }
+
+        private void FindStatistics()
+        {
+            double tempR;
+            for (int i = 0; i < numberOfExperiments; i++)
+            {
+                Generator();
+                tempR = R;
+                for (int j = 0; j < probs.Count; j++)
+                {
+                    tempR -= probs[j];
+                    if (tempR <= 0)
+                    {
+                        stat[j]++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private double FindAverage()
+        {
+            double average = 0;
+            for (int i = 0; i < stat.Count; i++)
+            {
+                average += stat[i] / numberOfExperiments * (i + 1);
+            }
+            return average;
+        }
+
+        private double FindVariance(double average)
+        {
+            double variance = 0;
+            for (int i = 0; i < stat.Count; i++)
+            {
+                variance += stat[i] / numberOfExperiments * (i + 1) * (i + 1);
+            }
+            variance -= average * average;
+            return variance;
+        }
+
+        private double FindChi()
+        {
+            double Chi = 0;
+            for (int i = 0; i < probs.Count; i++)
+            {
+                if (probs[i] != 0)
+                    Chi += (stat[i] * stat[i]) / (numberOfExperiments * probs[i]);
+            }
+            Chi -= numberOfExperiments;
+            return Chi;
+        }
+
+        private void Generator() // метод вычетов. Модификация Коробова
+        {
+            R = U / p;
+            U = (U * M) % p;
+        }
+    }
+}
